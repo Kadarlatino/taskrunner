92738c83bf9eb91dcc86789eb225ff2060fcf2f7
amalyshev <Alexander Malyshev>
2018-10-14 22:11:08 +0300

    dev-11 - some updates

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 let express = require('express'),
     router = express.Router(),
+    pathProf = '/profile',
     pathReg = '/register',
     pathLog = '/login';
 
@@ -8,9 +9,11 @@ router.get(pathReg, (req, res) => require('../controllers/userCtrl').get(req, re
 router.post(pathReg, (req, res) => require('../controllers/userCtrl').post(req, res));
 
 // Login
-
 router.get(pathLog, (req, res) => require('../controllers/userCtrl').get(req, res));
 router.post(pathLog, (req, res) => require('../controllers/userCtrl').post(req, res));
 
+// Profile
+router.get(pathProf, (req, res) => require('../controllers/userCtrl').get(req, res));
+router.post(pathProf, (req, res) => require('../controllers/userCtrl').post(req, res));
 
 module.exports = router;
